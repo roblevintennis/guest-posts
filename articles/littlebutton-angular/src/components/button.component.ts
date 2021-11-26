c037ca0d344001aada27a456c95661c1b6934f69
Rob Levin <rob.levin@ciphertrace.com>
2021-11-26 07:03:53 -0600

    Adds primary mode support in Angular implementation.

--- articles/littlebutton-angular/src/components/button.component.ts
+++ articles/littlebutton-angular/src/components/button.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'little-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() mode: 'primary' | undefined = undefined;
+
+  public get classes(): string {
+    const modeClass = this.mode ? `btn-${this.mode}` : '';
+    return [
+      'btn',
+      modeClass,
+    ].filter(cl => cl.length).join(' ');
+  }
+
+}
